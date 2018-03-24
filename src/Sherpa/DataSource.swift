ff869c33949e521598fcae1b174f197ba75c14ee
mobileflowllc <fieldforceapp@gmail.com>
2018-03-24 12:32:47 -0700

    Clean compile

--- src/Sherpa/DataSource.swift
+++ src/Sherpa/DataSource.swift
@@ -230,7 +230,7 @@ internal class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate 
 				var alpha: CGFloat = 0
 				foregroundColor?.getRed(nil, green: nil, blue: nil, alpha: &alpha)
 				if let foregroundColor = foregroundColor?.withAlphaComponent(0.85) {
-					attributedTitle.addAttribute(NSForegroundColorAttributeName, value: foregroundColor, range: NSMakeRange(0, attributedTitle.length))
+                    attributedTitle.addAttribute(NSAttributedStringKey.foregroundColor, value: foregroundColor, range: NSMakeRange(0, attributedTitle.length))
 				}
 				
 				var i = 0
@@ -240,9 +240,9 @@ internal class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate 
 					
 					if range.location == NSNotFound { break }
 					
-					attributedTitle.addAttribute(NSFontAttributeName, value: UIFont(descriptor: bold!, size: 0.0), range: range)
+                    attributedTitle.addAttribute(NSAttributedStringKey.font, value: UIFont(descriptor: bold!, size: 0.0), range: range)
 					if let foregroundColor = foregroundColor {
-						attributedTitle.addAttribute(NSForegroundColorAttributeName, value: foregroundColor, range: range)
+                        attributedTitle.addAttribute(NSAttributedStringKey.foregroundColor, value: foregroundColor, range: range)
 					}
 					
 					i = range.location + range.length
